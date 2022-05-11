38a64b51f6b73e9b3305c3b38c130a1bf2b4b06a
Elian-beep <elianoliver101@gmail.com>
2022-05-11 16:57:42 -0300

    Discussions: Area de criar novo tópico finalizado e com responsividade para mobile

--- Scripts/controlNewDiscussion.js
+++ Scripts/controlNewDiscussion.js
@@ -1,10 +1,24 @@
 let areaPNT = document.getElementById('areaPNT');
 let btnPNT = document.getElementById('btnPNT');
 
+
 let areaHNT = document.getElementById('areaHNT');
 let btnHNT = document.getElementById('btnHNT');
+
+let btnPsNT = document.getElementById('btnPsNT');
+let areaPsNT = document.getElementById('areaPsNT');
 
 btnPNT.addEventListener('click', (e) => {
     areaHNT.style.display = 'block';
     areaPNT.style.display = 'none';
+});
+
+btnHNT.addEventListener('click', (e) => {
+    areaPsNT.style.display = 'flex';
+    areaHNT.style.display = 'none';
+})
+
+btnPsNT.addEventListener('click', (e) => {
+    areaHNT.style.display = 'block';
+    areaPsNT.style.display = 'none';
 });
