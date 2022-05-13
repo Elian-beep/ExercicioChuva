680ce0d06e96a2e0445cf3191b89e66dfb1c2955
Elian-beep <elianoliver101@gmail.com>
2022-05-12 21:35:44 -0300

    Discussões: todos os cards exibem seus comentários + otimização do modo de publicar novo tópico

--- Scripts/controlNewDiscussion.js
+++ Scripts/controlNewDiscussion.js
@@ -1,38 +1,24 @@
-let areaPNT = document.getElementById('areaPNT');
-let btnPNT = document.getElementById('btnPNT');
+// let areaPNT = document.getElementById('areaPNT');
+// let btnPNT = document.getElementById('btnPNT');
 
 
-let areaHNT = document.getElementById('areaHNT');
-let btnHNT = document.getElementById('btnHNT');
+// let areaHNT = document.getElementById('areaHNT');
+// let btnHNT = document.getElementById('btnHNT');
 
-let btnPsNT = document.getElementById('btnPsNT');
-let areaPsNT = document.getElementById('areaPsNT');
+// let btnPsNT = document.getElementById('btnPsNT');
+// let areaPsNT = document.getElementById('areaPsNT');
 
-let areaCards2 = document.getElementById('areaCards2');
-let areaComments = document.getElementById('areaComments');
-let comments = false;
+// btnPNT.addEventListener('click', (e) => {
+//     areaPNT.style.display = 'none';
+//     areaHNT.style.display = 'block';
+// });
 
-btnPNT.addEventListener('click', (e) => {
-    areaHNT.style.display = 'block';
-    areaPNT.style.display = 'none';
-});
+// btnHNT.addEventListener('click', (e) => {
+//     areaHNT.style.display = 'none';
+//     areaPsNT.style.display = 'flex';
+// })
 
-btnHNT.addEventListener('click', (e) => {
-    areaPsNT.style.display = 'flex';
-    areaHNT.style.display = 'none';
-})
-
-btnPsNT.addEventListener('click', (e) => {
-    areaHNT.style.display = 'block';
-    areaPsNT.style.display = 'none';
-});
-
-areaCards2.addEventListener('click', (e) => {
-    if(!comments){ //Se os comentários estiverem escondidos 
-        areaComments.style.display = 'flex';
-        comments = true;
-    }else if(comments){
-        areaComments.style.display = 'none';
-        comments = false;
-    }
-});
+// btnPsNT.addEventListener('click', (e) => {
+//     areaPsNT.style.display = 'none';
+//     areaHNT.style.display = 'block';
+// });
---
+++ Scripts/controllNewDiscussionUpdate.js
@@ -0,0 +1,38 @@
+function showNewStep(idHidden, idShow, displayHidden, displayShow) {
+    /* idHidden: id da div que será escondido
+        idShow: id da div que será exibido
+        
+        displayHidden: forma que a div irá se esconder
+        displayShow: forma que a div irá aparecer*/
+
+    document.getElementById(idHidden).style.display = displayHidden;
+    document.getElementById(idShow).style.display = displayShow;
+}
+
+let areaCards2 = document.getElementById('areaCards2');
+let areaComments = document.getElementById('areaComments');
+let comments2 = false;
+
+let areaCards1 = document.getElementById('areaCards1');
+let areaComments1 = document.getElementById('areaComments1');
+let comments1 = false;
+
+areaCards1.addEventListener('click', (e) => {
+    if (!comments1) { //Se os comentários estiverem escondidos 
+        areaComments1.style.display = 'flex';
+        comments1 = true;
+    } else if (comments1) {
+        areaComments1.style.display = 'none';
+        comments1 = false;
+    }
+});
+
+areaCards2.addEventListener('click', (e) => {
+    if (!comments2) { //Se os comentários estiverem escondidos 
+        areaComments.style.display = 'flex';
+        comments2 = true;
+    } else if (comments2) {
+        areaComments.style.display = 'none';
+        comments2 = false;
+    }
+});
